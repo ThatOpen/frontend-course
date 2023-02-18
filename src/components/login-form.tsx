4ed6e36eb35711ea0585df6630d93e1bdfe83ccb
Antonio <antoniogviegas@hotmail.com>
2023-02-18 18:13:12 +0100

    Add firebase to project

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
+import { getApp } from "firebase/app";
 
 export const LoginForm: FC = () => {
-  return <h1>Hello login!</h1>;
+  return <p>{JSON.stringify(getApp())}</p>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { initializeApp } from "firebase/app";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjR_ivQkdToY7uyeVWgyXld1Awl1jcSsQ",
+  authDomain: "ifcjs-frontend-exercise.firebaseapp.com",
+  projectId: "ifcjs-frontend-exercise",
+  storageBucket: "ifcjs-frontend-exercise.appspot.com",
+  messagingSenderId: "490019428851",
+  appId: "1:490019428851:web:9b912649cc47d7929c9a86",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 root.render(
   <React.StrictMode>
